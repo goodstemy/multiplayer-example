f5996b78b5c9bdc3518a984716b25394d4f4eabd
goodstemy <baho.39rus@gmail.com>
2017-05-11 01:38:06 +0200

    Change port for heroku deploy

--- server.js
+++ server.js
@@ -3,16 +3,30 @@ const app     = require('express')();
 const server  = require('http').Server(app);
 const io      = require('socket.io')(server);
 
+const PORT = process.env.PORT || 3000;
+
 app.use('/', express.static(__dirname + '/'));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+let cache = null;
+
 io.on('connection', (socket) => {
   socket.on('client coordinates', (msg) => {
-    io.emit('server response', msg);
+    const parsedMsg = JSON.parse(msg);
+
+    if (!!parsedMsg.element && !cache) {
+      cache = msg;
+      io.emit('server response', cache);
+    } else if (!parsedMsg.element && !!cache) {
+      msg = cache;
+      io.emit('server response', msg);
+    } else {
+      io.emit('server response', msg);
+    }
   });
 });
 
-server.listen(3000);
+server.listen(PORT);
